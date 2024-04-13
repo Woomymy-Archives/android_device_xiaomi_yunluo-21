a21ae30ef54823b8af361d315e45ee57a81d3e72
bengris32 <bengris32@protonmail.ch>
2024-04-13 23:26:11 +0100

    yunluo: Inherit common MediaTek framework compatibility matrix
    Change-Id: I0a11f243c039c45091724e1b57d9bbcd1729f5f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,7 +182,7 @@ BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 3
 DEVICE_MANIFEST_FILE := $(CONFIGS_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(CONFIGS_PATH)/vintf/framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # VNDK
